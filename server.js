4effdf62202a7120933c18e8def6376c5374ed50
Tri Phan <minhtri11tk@gmail.com>
2019-11-18 11:06:55 -0600

    adding heroku

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds137263.mlab.com:37263/heroku_6bt8mhxw");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
